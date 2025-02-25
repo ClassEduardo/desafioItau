d71452518000dee795548b06b1cee7695854acbe
eduardo <eduardo.goncalves@grupobarigui.com.br>
2025-02-25 09:15:12 -0300

    First commit

---
+++ src/test/java/com/krtCompany/transacao_api/TransacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krtCompany.transacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
